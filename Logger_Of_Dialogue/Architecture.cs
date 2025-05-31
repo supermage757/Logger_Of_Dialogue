456ef48768a561528c6a8f3902a9367865cf59cd
tb9097 <tb9097@g.rit.edu>
2025-05-31 16:24:53 -0400

    Test push

---
+++ Logger_Of_Dialogue/Architecture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger_Of_Dialogue
+{
+    internal class Architecture
+    {
+        // ARCHITECTURE CLASS
+        // SOLELY MEANT FOR PLANNING OUT WHAT CLASSES ARE NEEDED
+
+        // First, test to make sure repo works and is connected.
+    }
+}
