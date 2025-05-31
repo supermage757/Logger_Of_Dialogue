0cd8af0d0f0756a8ae763b1dbe6aac7809ebf487
tb9097 <tb9097@g.rit.edu>
2025-05-31 16:19:58 -0400

    Add project files.

---
+++ Logger_Of_Dialogue/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Logger_Of_Dialogue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
