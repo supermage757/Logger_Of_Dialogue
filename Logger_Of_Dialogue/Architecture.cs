0072bca220ebcf777fdbb5f49de714bfadd2c130
tb9097 <tb9097@g.rit.edu>
2025-05-31 16:47:21 -0400

    Added more to architecture class, need to expand on what each individual class needs as far as methods, properties, etc etc before continuing with this code.

--- Logger_Of_Dialogue/Architecture.cs
+++ Logger_Of_Dialogue/Architecture.cs
@@ -23,5 +23,23 @@ namespace Logger_Of_Dialogue
             // People who deflect blame
             // People who downplay their own actions constantly
             // People who perpetually ask "when did i say/do that?" when confronted
+
+
+        // CLASSES NEEDED:::
+            // Utility class (guh doy) for all utility methods
+            // Class for the general logger software (including interface)
+            // Class to handle file saving and loading
+            // Dictionary class to handle appending/removing/altering entries
+            
+            // SIDE NOTE:: I'm honestly surprised with how little classes this needs
+            // It isn't that complex of a program, but that's okay
+
+
+        // STUFF EACH CLASS NEEDS:::
+        // UTILITY
+            // first character checker for user input
+                // check AS CAPITAL LETTERS for consistency! (toUpper)
+            // isnullorempty
+                // could possibly fold into the first character checker
     }
 }
