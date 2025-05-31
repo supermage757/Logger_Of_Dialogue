f96f0af42d7ece88c5dd062a6c05a8f6a5f62510
tb9097 <tb9097@g.rit.edu>
2025-05-31 16:28:05 -0400

    Began work on architecture.cs

--- Logger_Of_Dialogue/Architecture.cs
+++ Logger_Of_Dialogue/Architecture.cs
@@ -12,5 +12,16 @@ namespace Logger_Of_Dialogue
         // SOLELY MEANT FOR PLANNING OUT WHAT CLASSES ARE NEEDED
 
         // First, test to make sure repo works and is connected.
+        // Okay, this works. We smoove.
+
+        // I want this program to effectively:
+            // log statements/actions
+            // including the day/time of the statement/action
+            // and who made the statement/action
+
+        // This is made with manipulative people in mind
+            // People who deflect blame
+            // People who downplay their own actions constantly
+            // People who perpetually ask "when did i say/do that?" when confronted
     }
 }
