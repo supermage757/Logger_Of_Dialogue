e2e7019b56c148b8abf8a413dfe6629fcb540376
tb9097 <tb9097@g.rit.edu>
2025-06-10 14:35:47 -0400

    Utility class done and tested

--- Logger_Of_Dialogue/Utility.cs
+++ Logger_Of_Dialogue/Utility.cs
@@ -6,9 +6,29 @@ using System.Threading.Tasks;
 
 namespace Logger_Of_Dialogue
 {
-    internal class Utility
+    internal static class Utility
     {
         // First character checker
+        // Useful for Y/N inputs, as well as other necessary functions
+        internal static string FirstCharacterChecker(string text)
+        {
+            string firstChar = (text.Substring(0, 1)).ToUpper();
+            return firstChar;
+        }
+        // Tested, works as intended
+        // Works with both upper and lowercase characters
 
+        // Is null or empty
+        // Very straightforward
+        internal static bool NullOrNot(string text)
+        {
+            if (String.IsNullOrEmpty(text))
+                { return true; }
+            else
+                { return false; }
+        }
+        // Phew, almost made a major fucky here
+        // Works as intended though
+        
     }
 }
