0b25164040ff9e02641c26fb840d14e5b6c7c198
tb9097 <tb9097@g.rit.edu>
2025-06-10 14:11:37 -0400

    made utility class

---
+++ Logger_Of_Dialogue/Utility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger_Of_Dialogue
+{
+    internal class Utility
+    {
+        // First character checker
+
+    }
+}
