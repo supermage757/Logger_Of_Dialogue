dc2b0fbc56f1d09d13956b1732ba95f8c55b9ab6
tb9097 <tb9097@g.rit.edu>
2025-06-28 15:01:19 -0400

    Started general logger class

---
+++ Logger_Of_Dialogue/GeneralLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger_Of_Dialogue
+{
+    internal class GeneralLogger
+    {
+
+        // set up the whole dictionary thingy
+        // to verify if user wants to load or use new dictionary
+        internal bool LoadFromFile()
+        {
+            string tempChoice;
+            Console.WriteLine("Load from previously existing file? (Y/N)");
+            tempChoice = Console.ReadLine();
+            // get users choice
+            // if y then get file path
+        }
+    }
+}
